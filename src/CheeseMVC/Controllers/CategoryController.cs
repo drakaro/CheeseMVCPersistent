fb3c8d43bce7ea099f0e64f8918c19ff5902a306
Albert Young <albertkyoung@yahoo.com>
2018-03-01 14:07:20 -0800

    Finished coding part 3

--- src/CheeseMVC/Controllers/CategoryController.cs
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using CheeseMVC.Data;
 using CheeseMVC.Models;
 using CheeseMVC.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace CheeseMVC.Controllers
 {
